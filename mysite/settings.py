9885bcace55b3e73a9dd045d8006bbc54bbdd08c
Nitansh Bareja <nitansh.bareja@harman.com>
2017-03-25 12:13:32 +0530

    switching back to my db

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,10 +134,6 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-
 
 CELERY_RESULT_BACKEND = 'django-db'
 

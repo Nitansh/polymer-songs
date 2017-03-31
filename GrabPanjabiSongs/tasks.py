7beeaa7979098365d6748ce05e6e66a1b93dfbe3
Nitansh Bareja <nitansh.bareja@harman.com>
2017-04-01 02:48:34 +0530

    Fixing the Panjabi Grabber

--- GrabPanjabiSongs/tasks.py
+++ GrabPanjabiSongs/tasks.py
@@ -3,6 +3,7 @@ from celery import shared_task
 
 import urllib2
 import csv
+import zlib
 
 from .celery import app
 
@@ -11,22 +12,25 @@ from bs4 import BeautifulSoup
 from .models import PanjabiSong, PanjabiSongArtist, PanjabiSongAlbum
 
 request_headers = {
-"Host": "djraag.net",
+"Host": "djpunjab.com",
 "Connection": "keep-alive",
 "Cache-Control": "max-age=0",
 "Upgrade-Insecure-Requests": "1",
 "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
 "Accept-Language": "en,en-US;q=0.8",
-"Cookie": "__asc=d05a792d15a6cb153e1fc6c1aa0; __auc=d05a792d15a6cb153e1fc6c1aa0"
+"Accept-Encoding":"gzip, deflate, sdch",
+"Cookie": "__cfduid=d40d70b09afbaedefba64a10c6dc57d1d1490993987; __asc=e38c800715b262ac5e6be2cff88; __auc=e38c800715b262ac5e6be2cff88"
 }
 
-url_base  = "http://djraag.net"
+url_base  = "http://djpunjab.com/"
 
-url_base_hindi = "/latest-bollywood-movies-album-songs.html" 
+url_base_hindi = "punjabi_music/latest.php" 
 
 @app.task
 def fetch_page(page_no):
+    import pdb
+    pdb.set_trace()
     page_url = "%s%s?page=%d"%(url_base,url_base_hindi,page_no)
 
     request = urllib2.Request(page_url)
@@ -34,7 +38,8 @@ def fetch_page(page_no):
     for header in request_headers:
         request.add_header(header, request_headers[header])
     
-    web_page = urllib2.urlopen(request).read()
+    web_page = urllib2.urlopen(request)
+    web_page = zlib.decompress(web_page.read(), 16+zlib.MAX_WBITS)
     soup = BeautifulSoup(web_page)
 
     all_albums =  soup.findAll("p", { "class" : "dj" })
